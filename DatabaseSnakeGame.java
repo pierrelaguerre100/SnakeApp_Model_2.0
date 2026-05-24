40a26f2e12a78a4f1915cf0a28ead2f84b019272
husnica <husnica@cwu.edu>
2026-05-23 18:19:06 -0700

    Update DatabaseSnakeGame.java

--- DatabaseSnakeGame.java
+++ DatabaseSnakeGame.java
@@ -6,7 +6,7 @@ public class DatabaseSnakeGame {
     //database information
     private static final String URL = "jdbc:mysql://localhost:3306/snakegame";
     private static final String USER = "root";
-    private static final String PASSWORD = "ComputerScience380";
+    private static final String PASSWORD = "cs380";
 
     //connect to database
     public void connect() {
